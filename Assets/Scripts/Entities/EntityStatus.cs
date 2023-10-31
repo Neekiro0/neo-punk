60e19d9c5107d8c525a4fd0906a0aedf4a91fc0e
Wojciech Prusaczyk <woj.prusaczyk@gmail.com>
2023-10-31 19:46:53 +0100

    dopracowanie normalnych ataków, animacji i dodanie heavyAttack

---
+++ Assets/Scripts/Entities/EntityStatus.cs
@@ -0,0 +1,172 @@
+using System;
+using TMPro;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EntityStatus : MonoBehaviour
+{
+    public string entityName = "";
+    public int entityLevel = 1;
+    public int entityExperiencePoints = 0;
+    [ReadOnly] public int entityExperienceToNextLvl = 50;
+    public float entityHealthPoints = 100.0f;
+    public float entityMaxHelath = 100.0f;
+    public int gold = 0;
+    public float AttackDamage = 0;
+
+    private GameObject mainUserInterface;
+
+    private void Awake()
+    {
+        mainUserInterface = GameObject.Find("Main User Interface");
+    }
+
+    /*
+     * Nazwa
+     */
+    public void SetName(string name)
+    {
+        this.entityName = name;
+    }
+    public string GetName()
+    {
+        return this.entityName;
+    }
+    
+    /*
+     * Poziom doświadczenia
+     */
+    public void SetLevel(int level)
+    {
+        this.entityLevel = level;
+    }
+    public int GetLevel()
+    {
+        return this.entityLevel;
+    }
+    
+    /*
+     * Ilość punktów doświadczenia
+     */
+    public void SetXp(int xp)
+    {
+        this.entityExperiencePoints = xp;
+    }
+    public int GetXp()
+    {
+        return this.entityExperiencePoints;
+    }
+    public void AddXp(int xpAmount)
+    {
+        while (xpAmount > 0)
+        {
+            int xpToLvlUp = entityExperienceToNextLvl - GetXp();
+            
+            // Jeżeli nie mamy wystaczająco xp do lvl up
+            if (xpAmount < xpToLvlUp)
+            {
+                SetXp( GetXp() + xpAmount );
+                break;
+            }
+            if ( xpToLvlUp <= xpAmount )
+            {
+                SetLevel( GetLevel() + 1 );
+                SetExpToNextLVl(  Convert.ToInt32(Convert.ToDouble(GetExpToNextLVl()) * 1.25 ) );
+                xpAmount -= xpToLvlUp;
+                this.entityExperiencePoints = 0;
+            }
+        }
+    }
+    
+    /*
+     * Punkty życia
+     */
+    public void SetHp(float hp)
+    {
+        this.entityHealthPoints = hp;
+    }
+    public float GetHp()
+    {
+        return this.entityHealthPoints;
+    }
+    
+    /*
+     * Maksymalne punkty życia
+     */
+    public void SetMaxHp(float maxHp)
+    {
+        this.entityMaxHelath = maxHp;
+    }
+    public float GetMaxHp()
+    {
+        return this.entityMaxHelath;
+    }
+
+    public void DealDamage(float damage)
+    {
+        if ( damage >= GetHp() )
+        {
+            // Encja ginie
+            Debug.Log(entityName+" ginie!");
+            Destroy(gameObject);
+        } else if (damage < GetHp())
+        {
+            // encja otrzymuje obrażenia
+            SetHp(GetHp() - damage);
+            Debug.Log("Otrzymano " +damage.ToString()+ " obrażeń. Pozostało " +GetHp().ToString()+ "HP" );
+        }
+    }
+    
+    /*
+     * Punkty doświadczenia do następnego poziomu
+     */
+    public void SetExpToNextLVl(int expToNextLvl)
+    {
+        this.entityExperienceToNextLvl = expToNextLvl;
+    }
+    public int GetExpToNextLVl()
+    {
+        return this.entityExperienceToNextLvl;
+    }
+    
+    /*
+     * Ilość złota
+     */
+    public void SetGold(int gold)
+    {
+        this.gold = gold;
+    }
+    public int GetGold()
+    {
+        return this.gold;
+    }
+    public void AddGold(int gold)
+    {
+        this.gold += gold;
+
+        /*
+         * Jeśli encja jest graczem, to wyświetlamy złoto w UI
+         */
+        if ( gameObject.CompareTag("Player") )
+        {
+            GameObject UiGoldCount = mainUserInterface.transform.Find("Gold/Count").gameObject;
+            if (UiGoldCount)
+            {
+                UiGoldCount.GetComponent<TextMeshProUGUI>().text = Convert.ToString( this.gold );
+            }
+        }
+    }
+    
+    /*
+     * Obrażenia
+     */
+    public void SetAttackDamageCount(float AttackDamage)
+    {
+        this.AttackDamage = AttackDamage;
+    }
+    public float GetAttackDamageCount()
+    {
+        return this.AttackDamage;
+    }
+}
---
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -0,0 +1,333 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    
+    /*
+     * Zmienne dostępne w edytorze
+     */
+    [Tooltip("Set player speed, between 0 and 10000.")]
+    [Range(0, 100)]
+    public float playerSpeed = 20f;
+    
+    [Tooltip("Set player jump force, between 0 and 10000.")]
+    [Range(0, 30)]
+    public float jumpForce = 6f;
+
+    public bool isPlayerFacedRight = true;
+    public Animator animator;
+    public int attackState = 0; // Aktualny stan ataku
+    public bool isAttacking = false; // Czy trwa atak
+    public bool isGrounded = false; // Czy dotyka ziemii
+
+    private enum AttackSequence
+    {
+        First,
+        Second,
+        Third
+    }
+    
+    /*
+     * Zmienne lokalne
+     */
+    private Rigidbody2D playerBody;
+    private PlayerInventory playerEq;
+    private GameObject camera;
+    private bool isPassingThrough = false;
+    private Collider2D ignoredObject;
+    private CapsuleCollider2D boxCollider;
+    private GameObject rightSwordHitbox;
+    private GameObject leftSwordHitbox;
+    private float playerCurrentSpeed;
+    private Vector3 previousPosition;
+    private float attackTimeout = 0.8f; // Czas na zakończenie sekwencji ataku
+    private float lastAttackTime = 0; // aktualny pomiędzy atakami
+    private float attackCooldown = 0.4f; // cooldown ponmiędzy atakami
+    private Coroutine attackCoroutine;
+    private AttackSequence currentAttack = AttackSequence.First;
+    private FloorDetector FloorDetector;
+    public bool isChargingAttack = false;
+    public float keyHoldTime = 0.0f;
+    float holdTimeThreshold = 1.7f;
+    private EntityStatus playerStatus;
+    
+    private void Awake()
+    {
+        // pobieranie rigidbody
+        playerBody = GetComponent<Rigidbody2D>();
+        playerEq = gameObject.GetComponent<PlayerInventory>();
+        isPlayerFacedRight = true;
+        camera = transform.Find("Main Camera").gameObject;
+        FloorDetector = transform.Find("FloorDetector").gameObject.GetComponent<FloorDetector>();
+        boxCollider = GetComponent<CapsuleCollider2D>();
+        playerStatus = GetComponent<EntityStatus>();
+        GameObject swordHitboxes = transform.Find("SwordHitboxes").gameObject;
+        if (swordHitboxes)
+        {
+            rightSwordHitbox = swordHitboxes.transform.Find("RightHitbox").gameObject;
+            leftSwordHitbox = swordHitboxes.transform.Find("LeftHitbox").gameObject;
+        }
+    }
+
+
+    private void Update()
+    {
+
+        isGrounded = ( boxCollider.GetContacts(new ContactPoint2D[16]) > 0 );
+        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        
+        /*
+         * Przesyłanie odpowiednich zmiennych do animatora
+         */
+        animator.SetFloat("PlayerSpeed", Mathf.Abs(playerCurrentSpeed));
+        animator.SetFloat("PlayerVelocity", playerBody.velocity.y );
+        animator.SetInteger("PlayerAttackState", attackState );
+        animator.SetBool("IsPlayerAttacking", isAttacking );
+        animator.SetBool("IsGrounded", isGrounded );
+        animator.SetBool("IsChargingAttack", isChargingAttack );
+        animator.SetFloat("ChargingTime", keyHoldTime );
+        
+        if (isAttacking && (horizontalInput < 0 && isPlayerFacedRight))
+        {
+            horizontalInput = 0;
+        }
+        if (isAttacking && (horizontalInput > 0 && !isPlayerFacedRight) )
+        {
+            
+            horizontalInput = 0;
+        }
+        
+        /*
+         * przemieszczanie w osi x
+         */
+        if ( keyHoldTime < 0.3f )
+        {
+            transform.Translate(new Vector3(horizontalInput, 0, 0) * playerSpeed * Time.deltaTime);
+        }
+
+        // Oblicz prędkość poruszania się na podstawie zmiany pozycji między klatkami.
+        Vector3 currentPosition = transform.position;
+        Vector3 displacement = transform.position - previousPosition;
+        Vector3 speedVector = displacement / Time.deltaTime;
+        playerCurrentSpeed = speedVector.x;
+        previousPosition = currentPosition;
+        
+        /*
+         * Nieznaczne wydłużanie hitboxa ataków podczas biegu
+         */
+        if ( (Mathf.Abs(playerCurrentSpeed) > 3) || ( horizontalInput != 0 && isChargingAttack) )
+        {
+            rightSwordHitbox.GetComponent<BoxCollider2D>().size = new Vector2(1.4f, 0.25f);
+            leftSwordHitbox.GetComponent<BoxCollider2D>().size = new Vector2(1.4f, 0.25f);
+        }
+        else
+        {
+            rightSwordHitbox.GetComponent<BoxCollider2D>().size = new Vector2(0.35f, 0.25f);
+            leftSwordHitbox.GetComponent<BoxCollider2D>().size = new Vector2(0.35f, 0.25f);
+        }
+
+        /*
+         * skakanie
+         */
+        if ( (Input.GetKeyDown(InputManager.JumpKey) || Input.GetButtonDown(InputManager.PadButtonJump) ) && !isAttacking)
+        {
+            Jump();
+        }
+
+        /*
+         * Zmiana kierunku gracza
+         */
+        if ( Input.GetKey(InputManager.MoveLeftKey) && isPlayerFacedRight && (Time.timeScale != 0 ) && !isAttacking )
+        {
+            isPlayerFacedRight = false;
+        }
+        if (  Input.GetKey(InputManager.MoveRightKey) && !isPlayerFacedRight && (Time.timeScale != 0 ) && !isAttacking )
+        {
+            isPlayerFacedRight = true;
+        }
+        gameObject.GetComponent<SpriteRenderer>().flipX = !isPlayerFacedRight;
+        
+        
+        if (Input.GetKey(InputManager.AttackKey) && !isAttacking)
+        {
+            if (keyHoldTime < holdTimeThreshold)
+            {
+                keyHoldTime += Time.deltaTime;
+                isChargingAttack = true;
+            }
+        }
+
+        if (Input.GetKeyUp(InputManager.AttackKey))
+        {
+            if (isChargingAttack)
+            {
+                if (keyHoldTime >= holdTimeThreshold)
+                {
+                    PerformChargeAttack();
+                }
+                else
+                {
+                    // Rozpocznij atak od początku sekwencji
+                    StartAttack();
+                }
+            }
+            else if (isGrounded && isAttacking)
+            {
+                // Gracz kontynuuje sekwencję ataku
+                ContinueAttack();
+            }
+            isChargingAttack = false;
+            keyHoldTime = 0.0f;
+        }
+        
+        /*
+         * Przejście przez podłoże
+         */
+        if ( FloorDetector.isFloorPassable && isGrounded && Input.GetKeyDown(InputManager.MoveDownKey) )
+        {
+            DisableCollisionForDuration(0.3f);
+        }
+    }
+
+    private void Jump()
+    {
+        Vector2 jumpVector = new Vector2(0, jumpForce * 10);
+        float playerBodyVelocity = playerBody.GetPointVelocity(jumpVector).y;
+
+        if ( playerBodyVelocity == 0 && !playerEq.isEquipmentShown)
+        {
+            playerBody.AddForce(jumpVector, ForceMode2D.Impulse);
+        }
+    }
+    private void PerformChargeAttack()
+    {
+        if (isChargingAttack && ( keyHoldTime >= holdTimeThreshold ) )
+        {
+            //Debug.Log(keyHoldTime.ToString() + "....." + holdTimeThreshold.ToString());
+            if ( Input.GetAxis("Horizontal") != 0 )
+            {
+                DealDamage( playerStatus.GetAttackDamageCount() * 2 );
+            }
+            else
+            {
+                DealDamage( playerStatus.GetAttackDamageCount() * 3 );
+            }
+            animator.Play("heavyAttack_2");
+        }
+    }
+    
+    private void DisableCollisionForDuration(float duration)
+    {
+        ignoredObject = FloorDetector.collidingObject.GetComponent<Collider2D>();
+        
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), ignoredObject, true);
+        Invoke("EnableCollision", duration);
+    }
+
+    // Włączenie kolizji ponownie.
+    private void EnableCollision()
+    {
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), ignoredObject, false);
+        ignoredObject = null;
+    }
+
+    private void DealDamage(float damageToDeal)
+    {
+        if (isPlayerFacedRight)
+        {
+            // sprawdzanie czy gracz atakuje przeciwnika
+            GameObject collidingObject = rightSwordHitbox.gameObject.GetComponent<HitboxBehaviour>().collidingEnemyObject;
+            if (collidingObject)
+            {
+                // zadawanie obrażeń
+                collidingObject.GetComponent<EntityStatus>().DealDamage(damageToDeal);
+                rightSwordHitbox.gameObject.GetComponent<ParticleSystem>().Play();
+            }
+        }
+        else if (!isPlayerFacedRight)
+        {
+            // sprawdzanie czy gracz atakuje przeciwnika
+            GameObject collidingObject = leftSwordHitbox.gameObject.GetComponent<HitboxBehaviour>().collidingEnemyObject;
+            if (collidingObject)
+            {
+                // zadawanie obrażeń
+                collidingObject.GetComponent<EntityStatus>().DealDamage(damageToDeal);
+                leftSwordHitbox.gameObject.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+
+    private void StartAttack()
+    {
+        attackCoroutine = StartCoroutine(AttackTimeout());
+        isAttacking = true;
+        currentAttack = AttackSequence.First;
+        attackState = 1;
+        animator.Play("Attack_1");
+        DealDamage(playerStatus.GetAttackDamageCount());
+        movePlayerOnAttack(0.5f);
+    }
+
+    private void ContinueAttack()
+    {
+        if (attackCoroutine != null)
+        {
+            StopCoroutine(attackCoroutine);
+        }
+        attackCoroutine = StartCoroutine(AttackTimeout());
+
+        // Sprawdź, czy minęło wystarczająco dużo czasu między atakami
+        if (Time.time - lastAttackTime >= attackCooldown)
+        {
+            if (currentAttack == AttackSequence.Third)
+            {
+                // Gracz zaczyna nową sekwencję ataku
+                currentAttack = AttackSequence.First;
+                attackState = 1;
+            }
+            else
+            {
+                // Kontynuuj sekwencję ataku
+                currentAttack++;
+                attackState++;
+                
+                if ( attackState != 0 ) animator.Play("Attack_"+attackState.ToString());
+                
+                DealDamage(playerStatus.GetAttackDamageCount() * 1.2f);
+                movePlayerOnAttack(0.5f);
+            }
+
+            // Aktualizuj czas ostatniego ataku
+            lastAttackTime = Time.time;
+        }
+    }
+
+    private IEnumerator AttackTimeout()
+    {
+        yield return new WaitForSeconds(attackTimeout);
+
+        // Przerwanie ataku po timeout
+        isAttacking = false;
+        attackState = 0;
+        currentAttack = AttackSequence.First;
+    }
+
+    private void movePlayerOnAttack(float howFar)
+    {
+        // delikatne przesunięcie gracza po ataku
+        if (isPlayerFacedRight)
+        {
+            Vector3 movement = new Vector3(1.0f * howFar * 1000, 0, 0);
+            playerBody.AddForce(movement);
+        }
+        else
+        {
+            Vector3 movement = new Vector3(-1.0f * howFar * 1000, 0, 0);
+            playerBody.AddForce(movement);
+        }
+    }
+}
