bfcf38a4a9e1fb39778b4af9478df44a28f67673
Wojciech Prusaczyk <woj.prusaczyk@gmail.com>
2023-11-07 01:43:27 +0100

    totalna refaktoryzacja kodu głównej postaci, naprawa pewnych błędów, wdrożenie bardziej odpowiednich rozwiązań, dodanie dynamicznej kamery, zmiana tła

--- Assets/Scripts/Entities/Player/Player.cs
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -9,8 +9,6 @@ public class Player : MonoBehaviour
     /*
      * Zmienne dostępne w edytorze
      */
-    [Tooltip("Set player jump force, between 0 and 10000.")]
-    [Range(0, 30)]
     public float jumpForce = 6f;
 
     public Animator animator;
@@ -18,31 +16,20 @@ public class Player : MonoBehaviour
     public bool isAttacking = false; // Czy trwa atak
     public bool isGrounded = false; // Czy dotyka ziemii
 
-    private enum AttackSequence
-    {
-        First,
-        Second,
-        Third
-    }
-    
     /*
      * Zmienne lokalne
      */
     private Rigidbody2D playerBody;
     private PlayerInventory playerEq;
-    private GameObject camera;
     private bool isPassingThrough = false;
     private Collider2D ignoredObject;
     private CapsuleCollider2D boxCollider;
-    private GameObject rightSwordHitbox;
-    private GameObject leftSwordHitbox;
-    private float playerCurrentSpeed = 1;
+    private GameObject swordHitbox;
     private Vector3 previousPosition;
     private float attackTimeout = 0.6f; // Czas na zakończenie sekwencji ataku
     private float lastAttackTime = 0; // aktualny pomiędzy atakami
     private float attackCooldown = 0.3f; // cooldown ponmiędzy atakami
     private Coroutine attackCoroutine;
-    private AttackSequence currentAttack = AttackSequence.First;
     private FloorDetector FloorDetector;
     private bool isChargingAttack = false;
     private float keyHoldTime = 0.0f;
@@ -58,22 +45,14 @@ public class Player : MonoBehaviour
         // pobieranie rigidbody
         playerBody = GetComponent<Rigidbody2D>();
         playerEq = gameObject.GetComponent<PlayerInventory>();
-        camera = transform.Find("Main Camera").gameObject;
         FloorDetector = transform.Find("FloorDetector").gameObject.GetComponent<FloorDetector>();
         boxCollider = GetComponent<CapsuleCollider2D>();
         playerStatus = GetComponent<EntityStatus>();
-        GameObject swordHitboxes = transform.Find("SwordHitboxes").gameObject;
-        if (swordHitboxes)
-        {
-            rightSwordHitbox = swordHitboxes.transform.Find("RightHitbox").gameObject;
-            leftSwordHitbox = swordHitboxes.transform.Find("LeftHitbox").gameObject;
-        }
+        swordHitbox = transform.Find("SwordHitbox").gameObject;
     }
-
 
     private void Update()
     {
-
         isGrounded = ( boxCollider.GetContacts(new ContactPoint2D[16]) > 0 );
         
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -81,7 +60,7 @@ public class Player : MonoBehaviour
         /*
          * Przesyłanie odpowiednich zmiennych do animatora
          */
-        animator.SetFloat("PlayerSpeed", Mathf.Abs(playerCurrentSpeed));
+        animator.SetFloat("PlayerSpeed", Mathf.Abs(playerBody.velocity.x));
         animator.SetFloat("PlayerVelocity", playerBody.velocity.y );
         animator.SetInteger("PlayerAttackState", attackState );
         animator.SetBool("IsPlayerAttacking", isAttacking );
@@ -105,30 +84,26 @@ public class Player : MonoBehaviour
         /*
          * przemieszczanie w osi x
          */
-        if ( keyHoldTime < 0.3f && !isBlocking )
+        if (keyHoldTime < 0.3f && !isBlocking)
         {
-            transform.Translate(new Vector3(horizontalInput, 0, 0) * playerStatus.GetMovementSpeed() * Time.deltaTime);
+            Vector2 movement = new Vector2(horizontalInput * playerStatus.GetMovementSpeed(), playerBody.velocity.y);
+            playerBody.velocity = movement;
         }
-
-        // Oblicz prędkość poruszania się na podstawie zmiany pozycji między klatkami.
-        Vector3 currentPosition = transform.position;
-        Vector3 displacement = transform.position - previousPosition;
-        Vector3 speedVector = displacement / Time.deltaTime;
-        playerCurrentSpeed = speedVector.x;
-        previousPosition = currentPosition;
         
         /*
          * Nieznaczne wydłużanie hitboxa ataków podczas biegu
          */
-        if ( (Mathf.Abs(playerCurrentSpeed) > 3) || ( horizontalInput != 0 && isChargingAttack) )
+        if ( (Mathf.Abs(playerBody.velocity.x) > 10) || ( horizontalInput != 0 && isChargingAttack) )
         {
-            rightSwordHitbox.GetComponent<BoxCollider2D>().size = new Vector2(1.4f, 0.25f);
-            leftSwordHitbox.GetComponent<BoxCollider2D>().size = new Vector2(1.4f, 0.25f);
+            BoxCollider2D swordCollider = swordHitbox.GetComponent<BoxCollider2D>();
+            swordCollider.size = new Vector2(1f, 0.25f);
+            swordCollider.offset = new Vector2(0.5f, 0f);
         }
         else
         {
-            rightSwordHitbox.GetComponent<BoxCollider2D>().size = new Vector2(0.35f, 0.25f);
-            leftSwordHitbox.GetComponent<BoxCollider2D>().size = new Vector2(0.35f, 0.25f);
+            BoxCollider2D swordCollider = swordHitbox.GetComponent<BoxCollider2D>();
+            swordCollider.size = new Vector2(0.35f, 0.25f);
+            swordCollider.offset = new Vector2(0.18f, 0f);
         }
 
         /*
@@ -145,12 +120,13 @@ public class Player : MonoBehaviour
         if ( Input.GetKey(InputManager.MoveLeftKey) && playerStatus.isFacedRight && (Time.timeScale != 0 ) && !isAttacking && !isBlocking )
         {
             playerStatus.isFacedRight = false;
+            transform.Rotate(new Vector3(0f, 180f, 0f));
         }
         if (  Input.GetKey(InputManager.MoveRightKey) && !playerStatus.isFacedRight && (Time.timeScale != 0 ) && !isAttacking && !isBlocking )
         {
             playerStatus.isFacedRight = true;
+            transform.Rotate(new Vector3(0f, 180f, 0f));
         }
-        gameObject.GetComponent<SpriteRenderer>().flipX = !playerStatus.isFacedRight;
         
         /*
          * Atak, oraz charge attack
@@ -218,10 +194,10 @@ public class Player : MonoBehaviour
     {
         Vector2 jumpVector = new Vector2(0, jumpForce * 10);
         float playerBodyVelocity = playerBody.GetPointVelocity(jumpVector).y;
-
-        if ( playerBodyVelocity == 0 && !playerEq.isEquipmentShown)
+        
+        if (playerBodyVelocity == 0 && !playerEq.isEquipmentShown) // Sprawdzamy, czy postać jest na ziemi
         {
-            playerBody.AddForce(jumpVector, ForceMode2D.Impulse);
+            playerBody.AddForce(Vector2.up * jumpForce * 10, ForceMode2D.Impulse);
         }
     }
 
@@ -266,27 +242,13 @@ public class Player : MonoBehaviour
 
     private void DealDamage(float damageToDeal)
     {
-        if (playerStatus.isFacedRight)
+        // sprawdzanie czy gracz atakuje przeciwnika
+        GameObject collidingObject = swordHitbox.gameObject.GetComponent<HitboxBehaviour>().collidingEnemyObject;
+        if (collidingObject)
         {
-            // sprawdzanie czy gracz atakuje przeciwnika
-            GameObject collidingObject = rightSwordHitbox.gameObject.GetComponent<HitboxBehaviour>().collidingEnemyObject;
-            if (collidingObject)
-            {
-                // zadawanie obrażeń
-                collidingObject.GetComponent<EntityStatus>().DealDamage(damageToDeal);
-                rightSwordHitbox.gameObject.GetComponent<ParticleSystem>().Play();
-            }
-        }
-        else if (!playerStatus.isFacedRight)
-        {
-            // sprawdzanie czy gracz atakuje przeciwnika
-            GameObject collidingObject = leftSwordHitbox.gameObject.GetComponent<HitboxBehaviour>().collidingEnemyObject;
-            if (collidingObject)
-            {
-                // zadawanie obrażeń
-                collidingObject.GetComponent<EntityStatus>().DealDamage(damageToDeal);
-                leftSwordHitbox.gameObject.GetComponent<ParticleSystem>().Play();
-            }
+            // zadawanie obrażeń
+            collidingObject.GetComponent<EntityStatus>().DealDamage(damageToDeal);
+            swordHitbox.gameObject.GetComponent<ParticleSystem>().Play();
         }
     }
     
@@ -295,7 +257,6 @@ public class Player : MonoBehaviour
     {
         attackCoroutine = StartCoroutine(AttackTimeout());
         isAttacking = true;
-        currentAttack = AttackSequence.First;
         attackState = 1;
         animator.Play("Attack_1");
         DealDamage(playerStatus.GetAttackDamageCount());
@@ -313,16 +274,14 @@ public class Player : MonoBehaviour
         // Sprawdź, czy minęło wystarczająco dużo czasu między atakami
         if (Time.time - lastAttackTime >= attackCooldown)
         {
-            if (currentAttack == AttackSequence.Third)
+            if (attackState == 3)
             {
                 // Gracz zaczyna nową sekwencję ataku
-                currentAttack = AttackSequence.First;
                 attackState = 1;
             }
             else
             {
                 // Kontynuuj sekwencję ataku
-                currentAttack++;
                 attackState++;
                 
                 if ( attackState != 0 ) animator.Play("Attack_"+attackState.ToString());
@@ -343,7 +302,6 @@ public class Player : MonoBehaviour
         // Przerwanie ataku po timeout
         isAttacking = false;
         attackState = 0;
-        currentAttack = AttackSequence.First;
     }
 
     private void movePlayerOnAttack(float howFar)
@@ -360,6 +318,4 @@ public class Player : MonoBehaviour
             playerBody.AddForce(movement);
         }
     }
-    
-    
 }
